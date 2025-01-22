6049126dcd971d3092265442b72b045ddda7a7b1
Marijo <mjmc12.22@gmail.com>
2025-01-22 12:07:01 -0600

    Proyecto terminado

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista= [];
+let elemento = document.getElementById('listaAmigos');
+function agregarAmigo(){
+    let nombre= document.getElementById('amigo').value;
+    if (!(nombre.trim() === "")){
+        lista.push(nombre);
+        agregar(nombre, elemento);
+    }else{
+        alert("Por favor, ingreese un nombre valido")
+    }
+}
+
+function sortearAmigo(){
+    const elementos = elemento.getElementsByTagName('li');
+    const elementosLista= Array.from(elementos);
+    let numeroGenerado =  Math.floor(Math.random()*elementosLista.length);
+    let sorteo = document.getElementById('resultado');
+    agregar("El amigo secreto sorteado es: "+ elementosLista[numeroGenerado].textContent, sorteo);
+    elemento.style.display = 'none';
+
+}
+
+function agregar(nombre, donde){
+    const nuevo = document.createElement('li');
+    nuevo.textContent= nombre;
+    donde.appendChild(nuevo);
+}
